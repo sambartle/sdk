df06ce43ada13f9299deff4b2db6ca0885525c3c
Lancer <luoyejiang0701@gl-inet.com>
2018-09-25 10:33:44 +0800

    Initial commit

---
+++ openwrt-sdk-ar71xx-generic_gcc-7.3.0_musl.Linux-x86_64/build_dir/target-mips_24kc_musl/linux-ar71xx_generic/linux-4.9.111/scripts/kconfig/.conf.cmd
@@ -0,0 +1,1 @@
+cmd_scripts/kconfig/conf := gcc  -o scripts/kconfig/conf scripts/kconfig/conf.o scripts/kconfig/zconf.tab.o -L/home/kyson/workspace/devlop/release/openwrt-18.06.git/staging_dir/host/lib 
---
+++ openwrt-sdk-ramips-mt76x8_gcc-7.3.0_musl.Linux-x86_64/build_dir/target-mipsel_24kc_musl/linux-ramips_mt76x8/linux-4.14.54/scripts/mod/.modpost.cmd
@@ -0,0 +1,1 @@
+cmd_scripts/mod/modpost := gcc  -o scripts/mod/modpost scripts/mod/modpost.o scripts/mod/file2alias.o scripts/mod/sumversion.o -L/home/kyson/workspace/devlop/release/openwrt-18.06.git/staging_dir/host/lib 
